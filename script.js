320d769b7f3f3ae9edf0fecc36e6f5241db6db25
chages16 <chages16@gmail.com>
2020-06-29 22:28:25 +0930

    Create 2 Ajax queries, one for the movie and 1 for actors, added to Anji's script

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 //TEST VARIABLE FOR SEARCH (change movie name here)
-var filmSearch = "harry potter"
+var filmSearch = "star trek"
 //Global Variables for GIPHY
 var gifLimit = 700
 var gifQueryURL = "https://api.giphy.com/v1/gifs/search?q=" + filmSearch + "&api_key=jPos9RSs2YFAD8HQVQCBt782M5HUNlT6&limit=" + gifLimit;
-console.log(gifQueryURL)
+console.log(gifQueryURL);
 var gifShuffleNumber = 0;
 var gifClasses = [".gif1", ".gif2", ".gif3", ".gif4"];
-var gifClasses2 = [".gif5", ".gif6", ".gif7"];
 
 //SHUFFLE FUNCTION
 function shuffle(array) {
@@ -47,7 +46,6 @@ $.ajax({
   shuffle(gifShuffleNumber);
   console.log(gifShuffleNumber);
   var i2 = 0
-  var i3 = 0
 
   //runs loop here
   for (var i = 0; i < gifresults.length; i++) {
@@ -55,9 +53,8 @@ $.ajax({
     // looks for all gifs that are not rated r
     if (gifresults[gifShuffleNumber[i]].rating !== "r") {
       if (gifresults[gifShuffleNumber[i]].title.indexOf("film") >= 0 | gifresults[gifShuffleNumber[i]].slug.indexOf("film") >= 0 | gifresults[gifShuffleNumber[i]].title.indexOf("movie") >= 0 | gifresults[gifShuffleNumber[i]].slug.indexOf("movie") >= 0) {
-        console.log(gifresults[1].rating)
         // looks for gifs that are 200px in height and less then 351px in width
-        if (gifresults[gifShuffleNumber[i]].images.fixed_height.width < 281) {
+        //if (gifresults[gifShuffleNumber[i]].images.fixed_height.width < 281) {
           var gifDiv = $("<div>").css("float", "none");
           var gifOutput = $("<img>");
           gifOutput.attr({ "src": gifresults[gifShuffleNumber[i]].images.fixed_height.url, "alt": gifresults[gifShuffleNumber[i]].title });
@@ -65,46 +62,12 @@ $.ajax({
           var gifNumber = gifClasses[i2];
           $(gifNumber).append(gifDiv);
           i2++;
-          if (i2 == 4) {
+          if (i2 == 2) {
             i = gifLimit;
-            i3 = 1
           }
-        }
-        console.log(i2)
-        console.log(i)
+       // }
       }
     }
-  }
-  //if function can't find 4 pics it searches for 3 larger ones
-  if (i3 == 0) {
-    $(".gif1").empty();
-    $(".gif2").empty();
-    $(".gif3").empty();
-    var i2 = 0
-      for (var i = 0; i < gifresults.length; i++) {
-        // looks for all gifs that are not rated r
-        if (gifresults[gifShuffleNumber[i]].rating !== "r") {
-          if (gifresults[gifShuffleNumber[i]].title.indexOf("film") >= 0 | gifresults[gifShuffleNumber[i]].slug.indexOf("film") >= 0 | gifresults[gifShuffleNumber[i]].title.indexOf("movie") >= 0 | gifresults[gifShuffleNumber[i]].slug.indexOf("movie") >= 0) {
-            console.log(gifresults[1].rating)
-            // looks for gifs that are 200px in height and less then 351px in width
-            if (gifresults[gifShuffleNumber[i]].images.fixed_height.width < 371) {
-              var gifDiv = $("<div>").css("float", "none");
-              var gifOutput = $("<img>");
-              gifOutput.attr({ "src": gifresults[gifShuffleNumber[i]].images.fixed_height.url, "alt": gifresults[gifShuffleNumber[i]].title });
-              gifDiv.append(gifOutput);
-              var gifNumber = gifClasses2[i2];
-              $(gifNumber).append(gifDiv);
-              i2++
-              if (i2 == 3) {
-                i = gifLimit;
-                i3 = 2
-              }
-            }
-            console.log(i2)
-            console.log(i)
-          }
-        }
-      }
   }
 });
 
@@ -159,7 +122,45 @@ function omdb(title) {
       imdbDiv.append(imdbIcon);
       metaDiv.append(metaCIcon);
       $("#icons").prepend(tomatoDiv, imdbDiv, metaDiv);
+      //gif Actor search
+      var gifActors = response.Actors;
+      gifActors = gifActors.split(',')
+      var gifQueryActors = "https://api.giphy.com/v1/gifs/search?q=" + gifActors[(Math.floor(Math.random() * 4))] + "&api_key=jPos9RSs2YFAD8HQVQCBt782M5HUNlT6&limit=" + gifLimit;
 
-    });
+      $.ajax({
+        url: gifQueryActors,
+        method: "GET"
+      }).then(function (response3) {
+        //creates a random number array the length of the limit to randomize the gif output
+        var gifShuffleNumber2 = [];
+        var gifresults2 = response3.data;
+        for (var nn = 0; nn < gifresults2.length; nn++) {
+          gifShuffleNumber2.push(nn);
+        }
+        shuffle(gifShuffleNumber2);
+        var ia2 = 0
+        for (var ia = 0; ia < gifresults2.length; ia++) {
+          // looks for all gifs that are not rated r
+          if (gifresults2[gifShuffleNumber2[ia]].rating !== "r") {
+           // if (gifresults2[gifShuffleNumber2[ia]].title.indexOf(filmSearch) >= 0 | gifresults2[gifShuffleNumber2[ia]].slug.indexOf(filmSearch) >= 0 | gifresults2[gifShuffleNumber2[ia]].title.indexOf("movie") >= 0 | gifresults2[gifShuffleNumber2[ia]].slug.indexOf("movie") >= 0) {
+              // looks for gifs that are 200px in height and less then 351px in width
+              if (gifresults2[gifShuffleNumber2[ia]].images.fixed_height.width < 281) {
+                var gifDiv2 = $("<div>").css("float", "none");
+                var gifOutput2 = $("<img>");
+                gifOutput2.attr({ "src": gifresults2[gifShuffleNumber2[ia]].images.fixed_height.url, "alt": gifresults2[gifShuffleNumber2[ia]].title });
+                gifDiv2.append(gifOutput2);
+                var gifNumber2 = gifClasses[ia2 + 2];
+                $(gifNumber2).append(gifDiv2);
+                ia2++
+                if (ia2 == 2) {
+                  ia = gifresults2.length;
+                }
+              }
+            //}
+          }
+        }
+      })
+    })
 }
+
 omdb(filmSearch);
